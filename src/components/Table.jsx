f9227417fa6efb60bc9b28dd0744b03162387a38
hachim-am <114307554+am-hachim@users.noreply.github.com>
2023-05-18 01:50:57 +0200

    add of delete and admin functions

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,52 +1,77 @@
-import {FaTrashAlt} from 'react-icons/fa';
+import { FaTrashAlt } from 'react-icons/fa';
 import axios from 'axios';
-
-
+import { useState } from 'react';
 
 const Table = ({ tBodyData }) => {
+  const [selectedRow, setSelectedRow] = useState(null);
 
-
-
-function deleteUser(id)
-{
+  function deleteUser(id) {
     try {
-        const response =  axios.delete(`https://flexit.systems/api/deleteUser/${id}`, {
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + localStorage.getItem('token'),
-          },
-        });
-console.log(response);  
+      const response = axios.delete(`https://flexit.systems/api/deleteUser/${id}`, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+      });
+      console.log(response);
     } catch (error) {
-        console.log(error);
-      }
-}
+      console.log(error);
+    }
+  }
 
-    return (
-      <table className="table table-striped table-bordered" id="myTable">
-        <thead className="thead-dark">
-          <tr>
-            <th>id</th>
-            <th>Nom</th>
-            <th>Email</th>
-            <th></th>
+  function makeUserAdmin(id) {
+    try {
+      const response = axios.put(`https://flexit.systems/api/makeAdmin/${id}`,null, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleClick(id) {
+    setSelectedRow(id);
+    deleteUser(id);
+  }
+
+  function handleClickAdmin(id) {
+    setSelectedRow(id);
+    makeUserAdmin(id);
+  }
+
+  return (
+    <table className="table table-striped table-bordered" id="myTable">
+      <thead className="thead-dark">
+        <tr>
+          <th>id</th>
+          <th>Nom</th>
+          <th>Email</th>
+          <th></th>
+          <th>Admin</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tBodyData.map((item) => (
+          <tr key={item.id} className={selectedRow === item.id ? 'table-danger' : ''}>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+            <td onClick={() => handleClick(item.id)}>
+              <FaTrashAlt className={selectedRow === item.id ? 'text-danger' : ''} />
+            </td>
+            <td onClick={() => makeUserAdmin(item.id)}>{item.isAdmin ? 'oui' : 'non'}</td>
 
           </tr>
-        </thead>
-        <tbody>
-          {tBodyData.map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.name}</td>
-              <td>{item.email}</td>
-              <td onClick={deleteUser(item.id)}><FaTrashAlt/></td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  };
-  
-  export default Table;
-  
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
